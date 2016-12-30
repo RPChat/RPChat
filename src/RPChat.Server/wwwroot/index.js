2f04a5cf9051f52ecdb7bc9b2435a871d4c05e24
TyRoXx <tyroxxxx@gmail.com>
2016-12-30 14:51:47 +0100

    remove global variables; fix formatting

--- src/RPChat.Server/wwwroot/index.js
+++ src/RPChat.Server/wwwroot/index.js
@@ -1,30 +1,31 @@
-﻿var socket;
-var uri = "ws://" + window.location.host + "/ws";
-var output;
-var text = "test echo";
-
-function write(s) {
+﻿function write(s, output) {
     var p = document.createElement("p");
     p.innerHTML = s;
     output.appendChild(p);
 }
 
-function doConnect() {
-    socket = new WebSocket(uri);
-    socket.onopen = function (e) { write("opened " + uri); doSend(); };
-    socket.onclose = function (e) { write("closed"); };
-    socket.onmessage = function (e) { write("Received: " + e.data); socket.close(); };
-    socket.onerror = function (e) { write("Error: " + e.data); };
+function doConnect(output) {
+    var uri = "ws://" + window.location.host + "/ws";
+    var socket = new WebSocket(uri);
+    socket.onopen = function (e) {
+        write("opened " + uri, output);
+        var text = "test echo";
+        write("Sending: " + text, output);
+        socket.send(text);
+    };
+    socket.onclose = function (e) {
+        write("closed", output);
+    };
+    socket.onmessage = function (e) {
+        write("Received: " + e.data, output);
+        socket.close();
+    };
+    socket.onerror = function (e) {
+        write("Error: " + e.data, output);
+    };
 }
 
-function doSend() {
-    write("Sending: " + text);
-    socket.send(text);
-}
-
-function onInit() {
+window.onload = function () {
     output = document.getElementById("output");
-    doConnect();
-}
-
-window.onload = onInit;
+    doConnect(output);
+};
